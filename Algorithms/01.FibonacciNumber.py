874e2c55ae41987843ecdbfc0fd7a3122343c631
Saifullah-Balghari <balgharisaifullah@gmail.com>
2024-02-25 22:17:26 +0500

    initial commit

--- Algorithms/01.FibonacciNumber.py
+++ Algorithms/01.FibonacciNumber.py
@@ -3,15 +3,15 @@
 # Algorithm for generating Fabonacci Numbers using for Loops.
 
     # STEP 1: Start.
-    # STEP 2: Initialize an empty list fibonacci_numbers to store Fibonacci numbers.
-    # STEP 3: Initialize variables num1 and num2 to 0 and 1, respectively, to represent the first two Fibonacci numbers.
+    # STEP 2: Initialize an empty list "fibonacci_numbers" to store Fibonacci numbers.
+    # STEP 3: Initialize variables "num1" and "num2" to 0 and 1, respectively, to represent the first two Fibonacci numbers.
     # STEP 4: Use a for loop to iterate 10 times (from 0 to 9).
     # STEP 5: Inside the loop:
-        # a. Calculate the next Fibonacci number num3 by adding num1 and num2.
-        # b. Append num3 to the fibonacci_numbers list.
-        # c. Update num1 to the value of num2.
-        # d. Update num2 to the value of num3.
-    # STEP 6: After the loop, print the fibonacci_numbers.
+        # a. Calculate the next Fibonacci number "num3" by adding "num1" and "num2".
+        # b. Append "num3" to the "fibonacci_numbers" list.
+        # c. Update "num1" to the value of "num2".
+        # d. Update "num2" to the value of "num3".
+    # STEP 6: After the loop, print the "fibonacci_numbers".
     # STEP 7: Stop.
 
 # Code
@@ -29,16 +29,16 @@ print(fibonacci_numbers)    # Output: [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 # Algorithm for generating Fabonacci Numbers using Functions Recursion.
 
     # STEP 1: Start.
-    # STEP 2: Initialize counter to 1 and fibonacci_numbers as an empty list.
-    # STEP 3: Define a function fiboNumGen with parameters num1 and num2.
-    # STEP 4: Check if counter is less than or equal to 10.
+    # STEP 2: Initialize "counter" to 1 and "fibonacci_numbers" as an empty list.
+    # STEP 3: Define a function fiboNumGen with parameters "num1" and "num2".
+    # STEP 4: Check if "counter" is less than or equal to 10.
     # STEP 5: If true, do the following:
-        # a. Calculate the next Fibonacci number num3 by adding num1 and num2.
-        # b. Append num3 to the fibonacci_numbers list.
-        # c. Update num2 to the value of num1 and num1 to the value of num3.
-        # d. Increment counter by 1.
-        # e. Call fiboNumGen recursively with updated num1 and num2.
-    # STEP 6: If counter exceeds 10, print the list fibonacci_numbers.
+        # a. Calculate the next Fibonacci number "num3" by adding "num1" and "num2".
+        # b. Append "num3" to the "fibonacci_numbers" list.
+        # c. Update "num2" to the value of "num1" and "num1" to the value of "num3".
+        # d. Increment "counter" by 1.
+        # e. Call fiboNumGen recursively with updated "num1" and "num2".
+    # STEP 6: If "counter" exceeds 10, print the list "fibonacci_numbers".
     # STEP 7: End.
 
 # Code
@@ -61,7 +61,7 @@ def fiboNumGen(num1, num2):
 fiboNumGen(1,0)     # Output: [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 # Finding the Nth Fabonacci number by F(n) = F(n-1) + F(n-2):
-# This formula uses a 0-based index. This means that to generate the Nth Fibonacci number, we must write a number lower then that.
+# This formula uses a 0-based index. This means that to generate the Nth Fibonacci number, we must give a value lower then that.
 
 def FindFaboNum(number):
     if number <= 1:
