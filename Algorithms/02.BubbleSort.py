69aa64469fe1660c3df908a79bc9ea6cb0b53ca8
Saifullah-Balghari <balgharisaifullah@gmail.com>
2024-03-05 19:40:00 +0500

    initial commit

---
+++ Algorithms/02.BubbleSort.py
@@ -0,0 +1,11 @@
+
+## Bubble Sort:
+my_array = [ 7,12,9,11,3 ]
+
+n = len(my_array)
+for i in range(n-1):
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+
+print("Sorted array:", my_array)
